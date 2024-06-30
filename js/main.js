3c4375a1c16f231e039a6f745ad04202fe762373
Ladoga12 <169102815+Ladoga12@users.noreply.github.com>
2024-06-30 13:18:53 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).on("pagecreate", function() {
-    // Calcular área do Triângulo
-    $("#triangulo-form").submit(function(event) {
-        event.preventDefault();
-        let base = parseFloat($("#triangulo-base").val());
-        let altura = parseFloat($("#triangulo-alt
